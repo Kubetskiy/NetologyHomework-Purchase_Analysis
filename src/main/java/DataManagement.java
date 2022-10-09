a4ff334cc3fe4fd6ecbdb41e99d39d86ea039ada
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-09 12:17:36 +0300

    Readme.md added

--- src/main/java/DataManagement.java
+++ src/main/java/DataManagement.java
@@ -10,9 +10,6 @@ public class DataManagement {
      * loadCategories() - формирование мапы категорий из файла categories.tsv
      */
     static Map<String, String> loadCategories() throws IOException {
-
-//        Map<String, String> ctgs = Map.of("булка", "еда", "курица", "еда");
-
         var file = new File("categories.tsv");
         Map<String, String> goodsByCategory = new HashMap<>();
         try (var fis = new FileInputStream(file)) {
@@ -22,22 +19,7 @@ public class DataManagement {
                 var productAndCategory = s.split("\t");
                 goodsByCategory.put(productAndCategory[0], productAndCategory[1]);
             }
-//            goodsByCategory.put("сарделька", "еда");
         }
         return goodsByCategory;
-/*
-        var outFile = new File("TEST.txt");
-        try (var writer = new FileWriter(outFile)) {
-            for (Map.Entry<String, String> ent : goodsByCategory.entrySet()) {
-                writer.write(ent.getKey()+" "+ent.getValue()+"\n");
-            }
-*/
-/*
-            for (Map.Entry<String, String> entry : ctgs.entrySet()) {
-                writer.write(entry.getKey()+" "+entry.getValue()+"\n");
-            }
-        }
-*/
-//        return goodsByCategory;
     }
 }
