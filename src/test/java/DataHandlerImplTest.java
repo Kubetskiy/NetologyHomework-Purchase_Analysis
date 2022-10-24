d9a3b4f7be40c1ca44e24e59e525d258154dc844
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-24 13:36:58 +0300

    Change SDK ver. to 11

--- src/test/java/DataHandlerImplTest.java
+++ src/test/java/DataHandlerImplTest.java
@@ -21,25 +21,24 @@ class DataHandlerImplTest {
         String s2 = "{\"title\": \"булка\", \"date\": \"2022.02.08\", \"sum\": 222}";
         String s3 = "{\"title\": \"курица\", \"date\": \"2022.02.08\", \"sum\": 222}";
         String s4 = "{\"title\": \"шапка\", \"date\": \"2022.03.08\", \"sum\": 555}";
-        String r2 = """
-                {
-                  "maxCategory": {
-                    "category": "одежда",
-                    "sum": 555
-                  },
-                  "maxYearCategory": {
-                    "category": "одежда",
-                    "sum": 555
-                  },
-                  "maxMonthCategory": {
-                    "category": "еда",
-                    "sum": 444
-                  },
-                  "maxDayCategory": {
-                    "category": "еда",
-                    "sum": 444
-                  }
-                }""";
+        String r2 = "{\n" +
+                    "  \"maxCategory\": {\n" +
+                    "    \"category\": \"одежда\",\n" +
+                    "    \"sum\": 555\n" +
+                    "  },\n" +
+                    "  \"maxYearCategory\": {\n" +
+                    "    \"category\": \"одежда\",\n" +
+                    "    \"sum\": 555\n" +
+                    "  },\n" +
+                    "  \"maxMonthCategory\": {\n" +
+                    "    \"category\": \"еда\",\n" +
+                    "    \"sum\": 444\n" +
+                    "  },\n" +
+                    "  \"maxDayCategory\": {\n" +
+                    "    \"category\": \"еда\",\n" +
+                    "    \"sum\": 444\n" +
+                    "  }\n" +
+                    "}";
         dh.addSale(s2);
         dh.addSale(s4);
         dh.addSale(s3);
